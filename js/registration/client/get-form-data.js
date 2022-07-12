572cd9bc963c422cd21c5b567d14124b5b67b01a
Aleksandr <aleksandrignashov42@gmail.com>
2022-07-12 21:34:53 +0300

    create transport data from form

---
+++ js/registration/client/get-form-data.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const fields = document.querySelectorAll("input");
+  var valuesInput = {};
+
+  fields.forEach((field) => {
+    const { name, value } = field;
+
+    valuesInput[name] = value;
+  });
+
+  let elem = valuesInput.map(el => el);
+});
