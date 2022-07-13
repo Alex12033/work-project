204ee07a633a01f95370f4fb9090ff28a42515b5
Aleksandr <aleksandrignashov42@gmail.com>
2022-07-13 12:31:32 +0300

    create error message with css and js

--- js/registration/client/get-form-data.js
+++ js/registration/client/get-form-data.js
@@ -1,5 +1,33 @@
 let form = document.querySelector("form");
 
+function displayErrorMsg(msg, display) {
+  let errorDiv = document.querySelector(".error-msg");
+
+  errorDiv.innerHTML = msg;
+
+  errorDiv.style.display = display; //defolt in css display:hide
+}
+
+function validateForm(data) {
+  for (elem in data) {
+    if (data[elem] == "") {
+      let input = document.querySelector(`[name=${elem}]`);
+      input.classList.add("error");
+
+      displayErrorMsg("PLease enter each field", "block");
+    } else {
+      let input = document.querySelector(`[name=${elem}]`);
+      input.classList.remove("error");
+
+      displayErrorMsg("PLease enter each field", "none");
+    }
+
+    
+      console.log(data[elem])
+    
+  }
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -11,5 +39,6 @@ form.addEventListener("submit", (e) => {
 
     valuesInput[name] = value;
   });
-  console.log(valuesInput);
+
+  validateForm(valuesInput);
 });
