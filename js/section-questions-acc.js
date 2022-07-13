26e5d025016f4805607014889ce5ca4d4c0aaac0
Alex <alex.fursal_m@outlook.com>
2022-07-13 17:53:58 +0300

    fix accordion panel

--- js/section-questions-acc.js
+++ js/section-questions-acc.js
@@ -1,11 +1,37 @@
-const accordionBlock = document.querySelector('.accordion');
-const accordionItems = document.querySelectorAll('.accordion__item');
-const accordionItem = document.querySelectorAll(
-  '.accordion__item_wrapper-header'
-);
+let acc = document.querySelector('.accordion');
 
-accordionItem.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.parentElement.classList.toggle('accordion__item--active');
-  });
-});
+let selectedItem;
+
+acc.onclick = function (event) {
+  let target = event.target;
+
+  while (target != this) {
+    if (target.classList == 'accordion__item') {
+      active(target);
+      return;
+    }
+    target = target.parentNode;
+  }
+};
+function active(node) {
+  if (selectedItem) {
+    selectedItem.classList.remove('accordion__item--active');
+  }
+  selectedItem = node;
+  selectedItem.classList.add('accordion__item--active');
+}
+
+
+//.................................................
+
+// const accordionBlock = document.querySelector('.accordion');
+// const accordionItems = document.querySelectorAll('.accordion__item');
+// const accordionItem = document.querySelectorAll(
+//   '.accordion__item_wrapper-header'
+// );
+
+// accordionItem.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     item.parentElement.classList.toggle('accordion__item--active');
+//   });
+// });
