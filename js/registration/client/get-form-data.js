c5b0002ed6ffe91c0205a33a26c4222ad4a1f063
Aleksandr <aleksandrignashov42@gmail.com>
2022-07-13 18:06:43 +0300

    crate validate empty fields

--- js/registration/client/get-form-data.js
+++ js/registration/client/get-form-data.js
@@ -1,30 +1,52 @@
 let form = document.querySelector("form");
 
-function displayErrorMsg(msg, display) {
+function displayErrorMsg(msg, display, elem) {
   let errorDiv = document.querySelector(".error-msg");
+
+  let input = document.querySelector(`[name=${elem}]`);
+  input.classList.add("error-input");
 
   errorDiv.innerHTML = msg;
 
-  errorDiv.style.display = display; //defolt in css display:hide
+  errorDiv.style.display = display; //defolt in css display:none
+
+  setTimeout(() => {
+    errorDiv.style.display = "none";
+    input.classList.remove("error-input");
+  }, 3000);
 }
 
 function validateForm(data) {
+  console.log(data);
   for (elem in data) {
-    if (data[elem] == "") {
-      let input = document.querySelector(`[name=${elem}]`);
-      input.classList.add("error");
-
-      displayErrorMsg("PLease enter each field", "block");
-    } else {
-      let input = document.querySelector(`[name=${elem}]`);
-      input.classList.remove("error");
-
-      displayErrorMsg("PLease enter each field", "none");
+    //validation empty fields
+    let pattern = /^\d{10}$/;
+    
+    if (data[elem] == false) {
+      displayErrorMsg("PLease enter each field", "block", elem);
     }
+    
+    
+    if (elem == "phone-number" && pattern.test(data[elem])) {
+      console.log("SSKJDBvjksdjkvbkj");
+    }
+    console.log(elem);
 
-    
-      console.log(data[elem])
-    
+    // //validations number phone
+    // if (
+    //   elem == "phone-number" &&
+    //   data[elem] != "" &&
+    //   /^[\d\+][\d\(\)\ -]{4,14}\d$/.test(data[elem])
+    // ) {
+    //   console.log("phone");
+    // } else {
+    //   displayErrorMsg("NUMBER", "block");
+    // }
+
+    //validation email
+    // if (elem == "email" && /^[\d\+][\d\(\)\ -]{4,14}\d$/.test(data[elem])) {
+    //   alert("ksnkjdvsknd");
+    // }
   }
 }
 
@@ -37,7 +59,7 @@ form.addEventListener("submit", (e) => {
   fields.forEach((field) => {
     const { name, value } = field;
 
-    valuesInput[name] = value;
+    valuesInput[name] = value == "" ? false : value;
   });
 
   validateForm(valuesInput);
