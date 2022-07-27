51c526091952ba804538d412a65e2a08d75a0dfe
Aleksandr <aleksandrignashov42@gmail.com>
2022-07-27 20:21:31 +0300

    create validation on email modal

--- js/modal-window/modal.js
+++ js/modal-window/modal.js
@@ -1,55 +1,57 @@
 // Modal
-
-const modalTrigger = document.querySelectorAll('[data-modal]'),
-modal = document.querySelector('.modal');
+const modalTrigger = document.querySelectorAll("[data-modal]"),
+  modal = document.querySelector(".modal");
 
 function openModal() {
-modal.classList.add('show');
-modal.classList.remove('hide'); // modal.classList.toggle('show'); Альтернативный вариант вывода модалки без скроллинга
-document.body.style.overflow = 'hidden';
-clearInterval(modalTimeId);
+  modal.classList.add("show");
+  modal.classList.remove("hide"); // modal.classList.toggle('show'); Альтернативный вариант вывода модалки без скроллинга
+  document.body.style.overflow = "hidden";
+  clearInterval(modalTimeId);
 }
 
-modalTrigger.forEach(btn => {
-btn.addEventListener('click', openModal);
+modalTrigger.forEach((btn) => {
+  btn.addEventListener("click", openModal);
 });
 
 function closeModal() {
-modal.classList.add('hide');
-modal.classList.remove('show'); // modal.classList.toggle('show'); Альтернативный вариант вывода модалки без скроллинга
-document.body.style.overflow = '';
-}  
-
-modal.addEventListener('click', (e) => {
-if (e.target === modal || e.target.getAttribute('data-close') == '') {
-closeModal();
+  modal.classList.add("hide");
+  modal.classList.remove("show"); // modal.classList.toggle('show'); Альтернативный вариант вывода модалки без скроллинга
+  document.body.style.overflow = "";
 }
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal || e.target.getAttribute("data-close") == "") {
+    closeModal();
+  }
 });
 
-document.addEventListener('keydown', (e) => {
-if (e.code === "Escape" && modal.classList.contains('show')) {
-closeModal();
-}
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape" && modal.classList.contains("show")) {
+    closeModal();
+  }
 });
 
 const modalTimeId = setTimeout(openModal, 3000);
 
 function showModalByScroll() {
-if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-openModal();
-window.removeEventListener('scroll', showModalByScroll);
-}
+  if (
+    window.pageYOffset + document.documentElement.clientHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    openModal();
+    window.removeEventListener("scroll", showModalByScroll);
+  }
 }
 
-window.addEventListener('scroll', showModalByScroll);
+window.addEventListener("scroll", showModalByScroll);
 
 //CONTACT US
 
-const contactUs = document.querySelector('.contact-us');
+const contactUs = document.querySelector(".contact-us");
 
-contactUs.addEventListener('click', () => {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-    clearInterval(modalTimeId);
+contactUs.addEventListener("click", () => {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  document.body.style.overflow = "hidden";
+  clearInterval(modalTimeId);
 });
