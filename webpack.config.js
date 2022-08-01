e780a57c39f8824c539fbd26c6bc9fb5c116b9e8
Aleksandr <aleksandrignashov42@gmail.com>
2022-08-01 12:51:13 +0300

    fix question section cursor and fix webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "/"),
-  mode: "development",
 
   entry: {
     main: path.resolve(__dirname, "./js/entryWebPack/main.js"),
     burger_menu: path.resolve(__dirname, "./js/entryWebPack/burgerMenu.js"),
     auth_data: path.resolve(__dirname, "./js/entryWebPack/authData.js"),
-    footer_input: path.resolve(__dirname, "./js/entryWebPack/getFooterInput.js"),
+    footer_input: path.resolve(
+      __dirname,
+      "./js/entryWebPack/getFooterInput.js"
+    ),
     login_form: path.resolve(__dirname, "./js/entryWebPack/loginForm.js"),
     sign_up_form: path.resolve(__dirname, "./js/entryWebPack/signUpForm.js"),
   },
@@ -19,32 +22,48 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
   },
-  watch: true, 
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+    compress: true,
+    port: 8080,
+  },
 
   devtool: "source-map",
 
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
-  }
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ["style-loader", "css-loader"],
-//       },
-//     ],
-//   },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
 
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: path.resolve(__dirname, "index.html"),
-//       chunks: ["./js/modules/timer.js"],
-//       scriptLoading: "defer",
-//     }),
-
-//     new HtmlWebpackPlugin({
-//       template: "./pages/form-registration/login/index.html",
-//     }),
-//   ],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false, // Включите, чтобы убрать предупреждения о конфликтующем порядке
+    }),
+    //     new HtmlWebpackPlugin({
+    //       template: path.resolve(__dirname, "index.html"),
+    //       chunks: ["./js/modules/timer.js"],
+    //       scriptLoading: "defer",
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //       template: "./pages/form-registration/login/index.html",
+    //     }),
+  ],
 };
